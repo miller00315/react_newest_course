01c79aa3c50509e582197b5ae74ae903a7a67889
Miller Cesar de Oliveira <miller@academiadapele.com.br>
2021-05-13 21:05:21 -0300

    Ajustando criaçao de snapshot

--- src/app/components/PostCard/PostCard.test.jsx
+++ src/app/components/PostCard/PostCard.test.jsx
@@ -1,5 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import {PostCard} from './index'
+
+const postMock = {
+    title: 'testTitle',
+    body: 'testBody',
+    id: 1,
+    cover: 'img/img.png'
+}
+
 describe('<PostCard />', () => {
-    it('Should render a button', () => {
-        
+    it('Should render a PostCard correctly', () => {
+        render(<PostCard post={postMock} />);
+
+        expect(screen.getByRole('img', {name: 'testTitle'})).toHaveAttribute('src', postMock.cover);
+
+        expect(screen.getByText('testTitle')).toBeInTheDocument();
+
+        expect(screen.getByText('testBody')).toBeInTheDocument();
+    });
+
+    it('Should match a snapshot', () => {
+      const { container } = render(<PostCard post={postMock} />);
+
+      expect(container.firstChild).toMatchSnapshot();
     });
 });

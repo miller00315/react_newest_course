361ba6e62257632a1cf8d50d62726397ad087d81
Miller Cesar de Oliveira <miller@academiadapele.com.br>
2021-06-27 19:29:38 -0300

    useReducer

--- src/app/templates/Home/index.jsx
+++ src/app/templates/Home/index.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import { AppContext } from '../../contexts/AppContext';
-import { Div } from '../../components/Div';
+import React, { useReducer } from 'react';
 import './styles.css';
 
+const globalState = {
+  title: 'title',
+  body: 'body',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'update':
+      return { ...state, counter: state.counter + 1, title: action.payload };
+
+    case 'reverseTitle':
+      return { ...state, title: state.title.split('').reverse().join('') };
+
+    default:
+      return { ...state };
+  }
+};
+
 export const Home = () => {
+  const [state, dispatch] = useReducer(reducer, globalState);
+
+  const { title, body, counter } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <p>{body}</p>
+      <button onClick={() => dispatch({ type: 'update', payload: new Date().toLocaleString('pt-BR') })}>Click</button>
+      <button onClick={() => dispatch({ type: 'reverseTitle' })}>Reverse</button>
+    </div>
   );
 };

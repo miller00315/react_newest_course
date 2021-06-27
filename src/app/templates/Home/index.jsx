0657300d6248d580d27df9ca9710aebc504a4c1d
Miller Cesar de Oliveira <miller@academiadapele.com.br>
2021-06-27 12:41:24 -0300

    Ajustando o salvamento de contexto

--- src/app/templates/Home/index.jsx
+++ src/app/templates/Home/index.jsx
@@ -1,80 +1,54 @@
-import P from 'prop-types';
-
+import React, { useContext, useState } from 'react';
 import './styles.css';
 
-import { Posts } from '../../components/Posts';
-import { Button } from '../../components/Button';
-import { TextInput } from '../../components/TextInput';
+const globalState = {
+  title: 'context title',
+  counter: 0,
+  body: 'context body',
+};
 
-import { loadPosts } from '../../utils/load-posts';
-import { useEffect, useState, useCallback } from 'react';
+const GlobalContext = React.createContext();
 
-export const Home = ({ postQuantity = 2 }) => {
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(postQuantity);
-  const [searchValue, setSearchValue] = useState('');
-
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-
-  const filteredPosts = searchValue
-    ? posts.filter((posts) => posts.title.toLowerCase().includes(searchValue.toLowerCase()))
-    : posts;
-
-  const handleLoadPosts = useCallback(
-    async (page) => {
-      const postsAndPhotos = await loadPosts();
-
-      setPosts(postsAndPhotos.slice(page, postsPerPage));
-      setAllPosts(postsAndPhotos);
-    },
-    [postsPerPage],
-  );
-
-  const handleTextChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
-  };
-
-  useEffect(() => {
-    handleLoadPosts(0, postsPerPage);
-  }, [handleLoadPosts, postsPerPage]);
-
-  const loadMorePosts = () => {
-    const nextPage = page + postsPerPage;
-
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-
-    posts.push(...nextPosts);
-
-    setPosts(posts);
-    setPage(nextPage);
-  };
-
+const Div = () => {
   return (
-    <section className="container">
-      <div className="search-container">
-        {!!searchValue && (
-          <>
-            <h1>Search value: {searchValue}</h1>
-          </>
-        )}
-
-        <TextInput value={searchValue} handleTextChange={handleTextChange} />
-      </div>
-
-      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-
-      {filteredPosts.length === 0 && <p>Não existem posts</p>}
-
-      <div className="button-container">
-        {!searchValue && <Button onClick={loadMorePosts} label={'Load more posts'} disabled={noMorePosts} />}
-      </div>
-    </section>
+    <div>
+      <H1 />
+      <P />
+    </div>
   );
 };
 
-Home.propTypes = {
-  postQuantity: P.number,
+const H1 = () => {
+  const globalContext = useContext(GlobalContext);
+
+  const {
+    contextState: { title, counter },
+  } = globalContext;
+
+  return (
+    <h1 className="App">
+      {title} {counter}
+    </h1>
+  );
+};
+
+const P = () => {
+  const globalContext = useContext(GlobalContext);
+
+  const {
+    contextState: { body },
+    setContextState,
+  } = globalContext;
+
+  return <p onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}>{body}</p>;
+};
+
+export const Home = () => {
+  const [contextState, setContextState] = useState(globalState);
+
+  return (
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
+  );
 };
